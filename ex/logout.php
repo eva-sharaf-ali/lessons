74e94c07fb9819431738fd147868ad972acd7fda
eva-sharaf-ali <eva.shar.al83@gmail.com>
2025-01-01 16:37:20 +0300

    Delete ex directory

--- ex/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // حذف جميع بيانات الجلسة
-session_destroy(); // إنهاء الجلسة
-header("Location: login.php"); // إعادة التوجيه إلى صفحة تسجيل الدخول
-exit;
-?>
